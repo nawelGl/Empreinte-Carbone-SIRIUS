ba4216c829fde874e4f87f220aed4b4b57ed1848
binekoo <ghazal.nawel@gmail.com>
2024-02-10 20:11:56 +0100

    Connexion BD
    Connexion a la BD fonctionnelle au clic sur le bouton, mais pas encore de insert

--- front-end/src/main/java/edu/ezip/ing1/pds/front/MainFrame.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/MainFrame.java
@@ -1,10 +1,19 @@
 package edu.ezip.ing1.pds.front;
 
+import edu.ezip.ing1.pds.backend.CoreBackendServer;
+import edu.ezip.ing1.pds.backend.RequestHandler;
+import edu.ezip.ing1.pds.commons.Request;
+import edu.ezip.ing1.pds.commons.Response;
+import edu.ezip.ing1.pds.business.server.XMartCityService;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class MainFrame extends JFrame implements ActionListener {
 
@@ -65,6 +74,8 @@ public class MainFrame extends JFrame implements ActionListener {
         getContentPane().add(panel);
         panel.add(button, BorderLayout.SOUTH);
 
+        System.out.println("Ok frame lancée !");
+
         setVisible(true);
     }
 
@@ -72,25 +83,67 @@ public class MainFrame extends JFrame implements ActionListener {
 
     public static void main(String[] args) {
         MainFrame frame =  MainFrame.getInstance();
-        frame.initialize();
 
     }
+
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        if(e.getSource() == button){
+//            try {
+//                //Test avec un echo :
+//                ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", "java -version");
+//                pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+//                pb.redirectError(ProcessBuilder.Redirect.INHERIT);
+//                Process process = pb.start();
+//                int exitCode = process.waitFor();
+//                System.out.println("Code de sortie de la commande : " + exitCode);
+//            } catch (Exception ex) {
+//                //TODO : ajouter un logger pour les messages d'erreur ?
+//                throw new RuntimeException(ex);
+//            }
+//        }
+//    }
+
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        if(e.getSource() == button){
+//            Runtime runtime = Runtime.getRuntime();
+//            try {
+//                runtime.exec("cd ..");
+//                runtime.exec("cd xmart-city-backend/target");
+//                runtime.exec("java -jar xmart-city-backend-1.0-SNAPSHOT-jar-with-dependencies.jar");
+//            } catch (IOException ex) {
+//                ex.printStackTrace();
+//                System.out.println("Fail");
+//            }
+//            System.out.println("yoooo");
+//        }
+//    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == button){
+            //appeler dispatch ?
+            CoreBackendServer server = null;
             try {
-                //Test avec un echo :
-                ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", "java -version");
-                pb.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-                pb.redirectError(ProcessBuilder.Redirect.INHERIT);
-                Process process = pb.start();
-                int exitCode = process.waitFor();
-                System.out.println("Code de sortie de la commande : " + exitCode);
+                server = new CoreBackendServer();
             } catch (Exception ex) {
-                //TODO : ajouter un logger pour les messages d'erreur ?
-                throw new RuntimeException(ex);
+                System.out.println(ex.getMessage());
             }
+            server.run();
+//            final Request request = new Request();
+//            final Connection connection;
+//            final RequestHandler rh = server.requestHandlers;
+//            connection = rh.getConnection();
+//            request.setRequestOrder("SELECT_ALL_STUDENTS");
+//            XMartCityService xmartCityService = XMartCityService.getInstance();
+//            try {
+//                final Response response = xmartCityService.dispatch(request, connection);
+//            } catch (Exception ex) {
+//                System.out.println(ex.getMessage())
+//            }
         }
     }
+
+
 }
