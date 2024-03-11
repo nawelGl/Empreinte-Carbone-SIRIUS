73ce7dce2ad18750583983d31120d81e45d58dfa
Nawel <ghazal.nawel@gmail.com>
2024-03-11 22:41:50 +0100

    suppression annotations classe Produits

--- xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Produits.java
+++ xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Produits.java
@@ -1,17 +1,10 @@
 package edu.ezip.ing1.pds.business.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
-
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-@JsonRootName(value = "produits")
 public class Produits {
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("produits")
     private Set<Produit> produits = new LinkedHashSet<>();
 
     public Set<Produit> getProduits() {
