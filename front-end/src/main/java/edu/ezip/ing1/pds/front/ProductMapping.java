79f0f3c149f6a34573e5d6fd5a1e7a341744c77e
Nawel <ghazal.nawel@gmail.com>
2024-03-16 02:41:01 +0100

    Update ProductMapping.java

--- front-end/src/main/java/edu/ezip/ing1/pds/front/ProductMapping.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/ProductMapping.java
@@ -3,6 +3,12 @@ package edu.ezip.ing1.pds.front;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
+import edu.ezip.ing1.pds.business.dto.Emplacement;
+import edu.ezip.ing1.pds.client.SelectEmplacementById;
+import edu.ezip.ing1.pds.commons.Request;
+
+import static java.lang.String.valueOf;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,11 +18,23 @@ public class ProductMapping implements ActionListener{
     JFrame productMappingFrame;
     private String productName = RechercheReference.product.getNomProduit();
     private String productColor = RechercheReference.product.getCouleur();
+
+    private Emplacement emplacement;
+    private int idEmplacement = RechercheReference.product.getIdEmplacement();
     //private String productAisle = RechercheReference.product.getIdEmplacement();
     private int textSize = 20;
     private int borderTop = 20;
 
     public ProductMapping(){
+        //Recherche de l'emplacement via l'idEmplacement du produit :
+        try {
+            Request request = new Request();
+            request.setRequestContent(valueOf(idEmplacement));
+            emplacement = SelectEmplacementById.launchSelectEmplacementById(request);
+        } catch(Exception e){
+            System.out.println("Erreur sur l'idEmplacement : " + e.getMessage());
+        }
+
         productMappingFrame  = new JFrame();
         productMappingFrame.setSize(Template.LONGUEUR, Template.LARGEUR);
         productMappingFrame.setTitle("Trouvez votre produit.");
@@ -51,21 +69,21 @@ public class ProductMapping implements ActionListener{
 
         //------------aisle label-----------
         JLabel aisleLabel = new JLabel();
-        aisleLabel.setText("Allée n° ");
+        aisleLabel.setText("Allée " + emplacement.getAllee());
         aisleLabel.setFont(new Font("Avenir", Font.BOLD, textSize));
         aisleLabel.setBorder(new EmptyBorder(borderTop, 0, 0, 0));
         aislePanel.add(aisleLabel);
 
         //------------shelf label-----------
         JLabel shelfLabel = new JLabel();
-        shelfLabel.setText("Rayon n° ");
+        shelfLabel.setText("Rayon n° " + emplacement.getRayon());
         shelfLabel.setFont(new Font("Avenir", Font.BOLD, textSize));
         shelfLabel.setBorder(new EmptyBorder(borderTop, 0, 0, 0));
         shelfPanel.add(shelfLabel);
 
         //------------floor label-----------
         JLabel floorLabel = new JLabel();
-        floorLabel.setText("Étage n° ");
+        floorLabel.setText("Étage n° " + emplacement.getEtage());
         floorLabel.setFont(new Font("Avenir", Font.BOLD, textSize));
         floorLabel.setBorder(new EmptyBorder(borderTop, 0, 0, 0));
         floorPanel.add(floorLabel);
