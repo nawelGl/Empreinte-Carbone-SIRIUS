56c2e6e902c5ae1a5a1ac7242fe3c72aea2235d5
Nawel <ghazal.nawel@gmail.com>
2024-03-19 09:11:38 +0100

    ajout catégories

--- front-end/src/main/java/edu/ezip/ing1/pds/front/MenPants.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/MenPants.java
@@ -5,46 +5,39 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 
-public class WomenPants extends CategoriesTemplate implements ActionListener {
+public class MenPants extends CategoriesTemplate implements ActionListener{
 
-    JButton jupes;
     JButton pantalons;
     JButton jeans;
-    JButton leggings;
     JButton shorts;
+    JButton pantalonsSurvetement;
 
-    public WomenPants(){
+    public MenPants(){
         super();
-
-        jupes = new JButton("Jupes");
-        setButtonStyle(jupes, width, height); 
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        mainPanel.add(jupes, gbc);
 
         pantalons = new JButton("Pantalons");
         setButtonStyle(pantalons, width, height); 
-        gbc.gridx = 1;
+        gbc.gridx = 0;
         gbc.gridy = 0;
         mainPanel.add(pantalons, gbc);
 
         jeans = new JButton("Jeans");
         setButtonStyle(jeans, width, height); 
-        gbc.gridx = 2;
+        gbc.gridx = 1;
         gbc.gridy = 0;
         mainPanel.add(jeans, gbc);
 
-        leggings = new JButton("Leggings");
-        setButtonStyle(leggings, width, height); 
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        mainPanel.add(leggings, gbc);
-
         shorts = new JButton("Shorts");
         setButtonStyle(shorts, width, height); 
-        gbc.gridx = 1;
+        gbc.gridx = 0;
         gbc.gridy = 1;
         mainPanel.add(shorts, gbc);
+
+        pantalonsSurvetement = new JButton("Pantalons de survêtement");
+        setButtonStyle(pantalonsSurvetement, width, height); 
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        mainPanel.add(pantalonsSurvetement, gbc);
     }
 
     @Override
@@ -52,6 +45,5 @@ public class WomenPants extends CategoriesTemplate implements ActionListener {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
-
     
 }
