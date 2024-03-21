6d3f59ed7878e8a26654f14027af0ad03bd7023b
seulgi <seulgi-shin@etu.u-pec.fr>
2024-03-21 09:41:09 +0100

    CM with main

--- xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Produit.java
+++ xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Produit.java
@@ -84,7 +84,7 @@ public class Produit {
     public String getCouleur() {
         return couleur;
     }
-    
+
     public void setCouleur(String couleur){this.couleur = couleur;}
 
     public String getTaille() {
