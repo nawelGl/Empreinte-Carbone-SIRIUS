21ae1d2241d7d6c696953d2b31f3e66047d82c98
Haj abouda Nawres <nawres.haj-abouda@etu.u-pec.fr>
2024-03-22 14:06:36 +0100

    version fonctionnelle

--- Scripts/Compilation-exec-front.sh
+++ Scripts/Compilation-exec-front.sh
@@ -1,12 +1,10 @@
 
 
 # Execution and compilation of the front end
-<<<<<<< HEAD
+
 cd ..
 mvn clean install
 cd front-end
-=======
-cd ../"front-end"
->>>>>>> 5ad59ebcbba0276477967c31084b1f80d7d60d54
+
 mvn compile
 mvn exec:java
--- Scripts/generate-jar-server.sh
+++ Scripts/generate-jar-server.sh
@@ -5,11 +5,4 @@ cd ../"xmart-city-backend"
 # Exécute la commande Maven pour construire le jar
 mvn clean install
 mvn clean package
-#----- Chat gpt pour la verif -------
-# Vérifiez si la construction a réussi
-if [ $? -eq 0 ]; then
-    echo "Le jar a été généré avec succès."
-else
-    echo "Erreur lors de la génération du jar."
-    exit 1
-fi
+
