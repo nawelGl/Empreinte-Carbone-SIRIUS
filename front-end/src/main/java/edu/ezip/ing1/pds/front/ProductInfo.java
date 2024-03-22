53706fa1389bb2b3333bf5483f6e98123fa4486f
Haj abouda Nawres <nawres.haj-abouda@etu.u-pec.fr>
2024-03-22 12:13:37 +0100

    retirer le script en double

--- front-end/src/main/java/edu/ezip/ing1/pds/front/ProductInfo.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/ProductInfo.java
@@ -78,11 +78,13 @@ public class ProductInfo implements ActionListener {
         suggestionPanel.setLayout(null);
         suggestionPanel.setBounds(180,380,1030,300);
         suggestionPanel.setBackground(Color.WHITE);
-        mainPanel.add(suggestionPanel);
+
 
         JLabel label2 = new JLabel("Suggestions ");
         label1.setBounds(450,5,100,50);
         suggestionPanel.add(label2);
+
+        mainPanel.add(suggestionPanel);
 
 
 
