84a51aa0f9a4e6f5f7c9fbb64c5c6b8f0e1b55f5
Haj abouda Nawres <nawres.haj-abouda@etu.u-pec.fr>
2024-03-22 12:09:17 +0100

    Netoyage fichiers+ correction scripts

--- Scripts/execute-server-from-local-in-VM.sh
+++ Scripts/execute-server-from-local-in-VM.sh
@@ -3,11 +3,3 @@
 eval $(ssh-agent)
 ssh-add $HOME/.ssh/id_rsa
 ssh toto@172.31.249.142 "cd Scripts && ./execute-main-server.sh"
-# Fonction pour arrêter le serveur sur la machine distante
-#function stop_server {
-   # echo "Arrêt du serveur sur la machine distante..."
-    #ssh  toto@172.31.249.142 "killall java"
-#}
-
-## Gestionnaire de signal pour intercepter SIGINT (^C) et appeler la fonction stop_server
-#trap stop_server SIGINT
