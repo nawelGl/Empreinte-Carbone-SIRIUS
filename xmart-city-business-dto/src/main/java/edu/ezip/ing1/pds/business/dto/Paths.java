f8f7bd1e969c22d97ceb3af3045f59a4fe0af0ab
nawelGl <ghazal.nawel@gmail.com>
2024-03-27 02:29:35 +0100

    Update Paths.java

---
+++ xmart-city-business-dto/src/main/java/edu/ezip/ing1/pds/business/dto/Paths.java
@@ -0,0 +1,9 @@
+package edu.ezip.ing1.pds.business.dto;
+
+import java.util.HashMap;
+
+public class Paths {
+    //Collection de tous les chemins reliés à leur ID.
+    HashMap<Integer, Path> capitalCities = new HashMap<Integer, Path>();
+    
+}
