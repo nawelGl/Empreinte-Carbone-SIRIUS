9b32aa2ff95af0937ac80202998fae15f3dc9ba3
nawelGl <ghazal.nawel@gmail.com>
2024-05-16 00:10:36 +0200

    Update CategoriesFrame.java

--- front-end/src/main/java/edu/ezip/ing1/pds/front/CategoriesFrame.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/CategoriesFrame.java
@@ -1,4 +1,24 @@
 package edu.ezip.ing1.pds.front;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class CategoriesFrame {
+    JFrame categorieFrame = new JFrame();
+
+    public CategoriesFrame(){
+        categorieFrame.setTitle("Choisissez un produit");
+        categorieFrame.setSize(Template.LONGUEUR, Template.LARGEUR);
+        categorieFrame.setResizable(false);
+        categorieFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        categorieFrame.setLocationRelativeTo(null);
+
+        //-------- title panel ---------
+        JPanel titlePanel = new JPanel();
+        titlePanel.setBackground(Color.decode(Template.COULEUR_SECONDAIRE));
+        titlePanel.setPreferredSize(new Dimension(Template.LONGUEUR, 100));
+        categorieFrame.add(BorderLayout.CENTER, titlePanel);
+
+        categorieFrame.setVisible(true);
+    }
 }
