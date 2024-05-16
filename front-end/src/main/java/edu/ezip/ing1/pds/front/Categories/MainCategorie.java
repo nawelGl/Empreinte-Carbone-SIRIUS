36fdd4f8b02ef660e284396097ad409b397471dc
nawelGl <ghazal.nawel@gmail.com>
2024-05-16 20:27:36 +0200

    MainCategorie.java : données en dur --> données récupérées en bd

--- front-end/src/main/java/edu/ezip/ing1/pds/front/Categories/MainCategorie.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/Categories/MainCategorie.java
@@ -2,56 +2,42 @@ package edu.ezip.ing1.pds.front.Categories;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import edu.ezip.ing1.pds.business.dto.Categorie;
 import javax.swing.JButton;
 
 public class MainCategorie extends CategoriesTemplate implements ActionListener{
-    //Buttons for each category :
-    JButton categorieFemme;
-    JButton categorieHomme;
-    JButton categorieEnfant;
 
     public MainCategorie(){
         super();
 
-        //Women category :
-        categorieFemme = new JButton("Femmes");
-        categorieFemme.addActionListener(this);
-        setButtonStyle(categorieFemme, width, height); 
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        mainPanel.add(categorieFemme, gbc);
-
-        //Men category :
-        categorieHomme = new JButton("Hommes");
-        categorieHomme.addActionListener(this);
-        setButtonStyle(categorieHomme, width, height);
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        mainPanel.add(categorieHomme, gbc);
-
-        //Child category :
-        categorieEnfant = new JButton("Enfants");
-        categorieEnfant.addActionListener(this);
-        setButtonStyle(categorieEnfant, width, height);
-        gbc.gridx = 2;
-        gbc.gridy = 0;
-        mainPanel.add(categorieEnfant, gbc);
-
+        for (int i = 0; i < categories.getCategories().size(); i++){
+            //Dans le acton liste,ner, differencier avec getText !
+            JButton bouton = new JButton();
+            Categorie categorie = categories.getCategories().get(i);
+            bouton.setText(categorie.getNomCategorie());
+            bouton.addActionListener(this);
+            setButtonStyle(bouton, width, height);
+            gbc.gridx = i;
+            gbc.gridy = 0;
+            mainPanel.add(bouton, gbc);
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         super.actionPerformed(e);
-        if(e.getSource() == categorieFemme){
-            WomenCategories womenCategories = new WomenCategories();
-            categorieFrame.dispose();
-        } else if(e.getSource() == categorieHomme){
-            MenCategories menCategories = new MenCategories();
-            categorieFrame.dispose();
-        } else if(e.getSource() == categorieEnfant){
-            ChildrenCategories childrenCategories = new ChildrenCategories();
-            categorieFrame.dispose();
+        if (e.getSource() instanceof JButton) {
+            JButton button = (JButton) e.getSource();
+            if(button.getText().equals("Hommes")){
+                MenCategories menCategories = new MenCategories();
+                categorieFrame.dispose();
+            } else if(button.getText().equals("Femmes")){
+                WomenCategories womenCategories = new WomenCategories();
+                categorieFrame.dispose();
+            } else if(button.getText().equals("Enfants")){
+                ChildrenCategories childrenCategories = new ChildrenCategories();
+                categorieFrame.dispose();
+            }
         } 
     }
     
