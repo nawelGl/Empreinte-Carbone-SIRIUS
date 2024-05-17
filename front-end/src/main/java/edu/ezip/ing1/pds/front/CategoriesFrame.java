28cc1fb4bcf5e5bfd3d2be13fd10c3150022241f
nawelGl <ghazal.nawel@gmail.com>
2024-05-17 10:54:37 +0200

    Update CategoriesFrame.java

--- front-end/src/main/java/edu/ezip/ing1/pds/front/CategoriesFrame.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/CategoriesFrame.java
@@ -1,15 +1,15 @@
 package edu.ezip.ing1.pds.front;
 
-import edu.ezip.ing1.pds.business.dto.Produit;
-import edu.ezip.ing1.pds.business.dto.Produits;
-import edu.ezip.ing1.pds.business.dto.SousCategorieB;
-import edu.ezip.ing1.pds.business.dto.SousCategoriesB;
+import edu.ezip.ing1.pds.business.dto.*;
 import edu.ezip.ing1.pds.client.Categories.SelectAllProductByCategorie;
+import edu.ezip.ing1.pds.client.Categories.SelectCategorieByID;
+import edu.ezip.ing1.pds.client.UC1.SelectMarqueById;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.io.IOException;
+import java.util.Objects;
 
 public class CategoriesFrame {
     JFrame categorieFrame = new JFrame();
@@ -18,6 +18,7 @@ public class CategoriesFrame {
     Produits produits;
     private int y = 0;
     SousCategorieB sous_categorie_B;
+    Marque marqueProduit = new Marque();
 
     public CategoriesFrame(SousCategorieB sous_categorie_B, int isSousCatA, int idCat){
         this.sous_categorie_B = sous_categorie_B;
@@ -70,23 +71,66 @@ public class CategoriesFrame {
 
         JPanel panelPanelProduct = new JPanel();
         panelPanelProduct.setBackground(Color.decode(Template.COULEUR_PRINCIPALE));
-        panelPanelProduct.setLayout(null);
-        // Créer un JScrollPane et y ajouter le panel
-        JScrollPane scrollPane = new JScrollPane();
+        panelPanelProduct.setLayout(new BoxLayout(panelPanelProduct, BoxLayout.Y_AXIS));
+        panelPanelProduct.setBorder(new EmptyBorder(20, 50, 20, 50));
+
+        // Créer le JScrollPane et y ajouter panelPanelProduct
+        JScrollPane scrollPane = new JScrollPane(panelPanelProduct);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 
-        // Ajouter le JScrollPane au cadre
-        panelPanelProduct.add(scrollPane);
-        mainPanel.add(panelPanelProduct);
+        // Ajouter le JScrollPane au mainPanel
+        mainPanel.add(BorderLayout.CENTER, scrollPane);
 
         //-------- faire un foreach pour faire un panel pour chaque produit avec ses infos
         for (int i = 0; i < produits.getProduits().size(); i++){
             JPanel productPanel = new JPanel();
+            productPanel.setLayout(null);
             productPanel.setBackground(Color.WHITE);
-            productPanel.setBounds(110, 20+y, 1200, 200);
+            productPanel.setBounds(110, 15+y, 1200, 200);
             panelPanelProduct.add(productPanel);
             y+=220;
+            Produit produit = produits.getProduits().get(i);
+
+            ImageIcon pictureProduct = new ImageIcon(Objects.requireNonNull(MethodesFront.class.getResource("/"+produit.getReference()+".png")));
+            Image image = pictureProduct.getImage();
+            Image nouvelleImage = image.getScaledInstance(150, 154, Image.SCALE_SMOOTH);
+
+            ImageIcon nouvelleIcon = new ImageIcon(nouvelleImage);
+
+            JLabel pictureProductLabel = new JLabel(nouvelleIcon);
+            pictureProductLabel.setBounds(20,20,150,154);
+            productPanel.add(pictureProductLabel);
+
+            JLabel nomProduit = new JLabel();
+            nomProduit.setText(produit.getNomProduit() + " ( référence : " + produit.getReference() + ")");
+            nomProduit.setFont(Template.FONT_ECRITURE2);
+            nomProduit.setBounds(220, 15, 400, 40);
+            productPanel.add(nomProduit);
+
+            try {
+                marqueProduit = SelectMarqueById.launchSelectMarqueById(String.valueOf(produit.getIdMarque()));
+            } catch (Exception e) {
+                System.out.println("Erreur sur la récupération du nom de la categorie ou de la marque  : " + e.getMessage());
+            }
+
+            JLabel marque = new JLabel();
+            marque.setText("Marque : " + marqueProduit.getNomMarque());
+            marque.setFont(Template.FONT_ECRITURE2);
+            marque.setBounds(220, 45, 200, 40);
+            productPanel.add(marque);
+
+            JLabel taille = new JLabel();
+            taille.setText("Taille : " + produit.getTaille());
+            taille.setFont(Template.FONT_ECRITURE2);
+            taille.setBounds(220, 75, 200, 40);
+            productPanel.add(taille);
+
+            JLabel prix = new JLabel();
+            prix.setText("Prix : " + produit.getPrix() + "€");
+            prix.setFont(Template.FONT_ECRITURE2);
+            prix.setBounds(220, 105, 200, 40);
+            productPanel.add(prix);
         }
 
         categorieFrame.setVisible(true);
