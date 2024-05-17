79b5d1eba2cc6a113ad3b22d881565393e1f689c
nawelGl <ghazal.nawel@gmail.com>
2024-05-17 10:13:02 +0200

    Ajout des panels à la frame des categories

--- front-end/src/main/java/edu/ezip/ing1/pds/front/CategoriesFrame.java
+++ front-end/src/main/java/edu/ezip/ing1/pds/front/CategoriesFrame.java
@@ -1,18 +1,39 @@
 package edu.ezip.ing1.pds.front;
 
+import edu.ezip.ing1.pds.business.dto.Produit;
+import edu.ezip.ing1.pds.business.dto.Produits;
 import edu.ezip.ing1.pds.business.dto.SousCategorieB;
 import edu.ezip.ing1.pds.business.dto.SousCategoriesB;
+import edu.ezip.ing1.pds.client.Categories.SelectAllProductByCategorie;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.io.IOException;
 
 public class CategoriesFrame {
     JFrame categorieFrame = new JFrame();
+    private int idSousCatA;
+    private int idCat;
+    Produits produits;
+    private int y = 0;
     SousCategorieB sous_categorie_B;
 
-    public CategoriesFrame(SousCategorieB sous_categorie_B){
+    public CategoriesFrame(SousCategorieB sous_categorie_B, int isSousCatA, int idCat){
         this.sous_categorie_B = sous_categorie_B;
+        this.idSousCatA = isSousCatA;
+        this.idCat = idCat;
+
+        String parametres = String.valueOf(idCat) + "," + String.valueOf(idSousCatA) + "," + String.valueOf(sous_categorie_B.getIdSousCategorieB());
+        try{
+            produits = SelectAllProductByCategorie.launchSelectAllProductsByCategorie(parametres);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(produits.toString());
 
         categorieFrame.setTitle("Choisissez un produit");
         categorieFrame.setSize(Template.LONGUEUR, Template.LARGEUR);
@@ -47,7 +68,26 @@ public class CategoriesFrame {
         categorieFrame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         mainPanel.add(BorderLayout.NORTH, mainTitre);
 
+        JPanel panelPanelProduct = new JPanel();
+        panelPanelProduct.setBackground(Color.decode(Template.COULEUR_PRINCIPALE));
+        panelPanelProduct.setLayout(null);
+        // Créer un JScrollPane et y ajouter le panel
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        // Ajouter le JScrollPane au cadre
+        panelPanelProduct.add(scrollPane);
+        mainPanel.add(panelPanelProduct);
+
         //-------- faire un foreach pour faire un panel pour chaque produit avec ses infos
+        for (int i = 0; i < produits.getProduits().size(); i++){
+            JPanel productPanel = new JPanel();
+            productPanel.setBackground(Color.WHITE);
+            productPanel.setBounds(110, 20+y, 1200, 200);
+            panelPanelProduct.add(productPanel);
+            y+=220;
+        }
 
         categorieFrame.setVisible(true);
     }
