7fc624315e6e2bd67fa0481bb47568700cb02f96
Haj abouda Nawres <nawres.haj-abouda@etu.u-pec.fr>
2024-05-19 11:01:11 +0200

    Suppression commentaires francais inutiles

--- front-end/src/test/java/edu/ezip/ing1/pds/front/UC1/MethodesFrontTest.java
+++ front-end/src/test/java/edu/ezip/ing1/pds/front/UC1/MethodesFrontTest.java
@@ -13,18 +13,18 @@ public class MethodesFrontTest {
     @Test
     public void testCarbonFootPrintCalcul() {
         // Given
-        double coordLat1 = 40.4168;  // Latitude de Madrid
-        double coordLong1 = -3.7038; // Longitude de Madrid
-        double coordLat2 = 48.8566;  // Latitude de Paris
-        double coordLong2 = 2.3522;  // Longitude de Paris
-        double coeff = 1605;         // Coefficient de conversion
-        double poids = 200;          // Poids en grammes
+        double coordLat1 = 40.4168;
+        double coordLong1 = -3.7038;
+        double coordLat2 = 48.8566;
+        double coordLong2 = 2.3522;
+        double coeff = 1605;         
+        double poids = 200;
 
         // When
         double actualCarbonFootPrint = MethodesFront.carbonFootPrintCalcul(coordLat1, coordLat2, coordLong1, coordLong2, coeff, poids);
 
         // Then
-        // Calcul manuel pour vérifier le résultat attendu
+       
         double distance = MethodesFront.getDistanceBetweenPointsNew(coordLat1, coordLong1, coordLat2, coordLong2);
         double expectedCarbonFootPrint = distance * coeff * (poids / 1000) / 1000;
         BigDecimal bd = new BigDecimal(expectedCarbonFootPrint).setScale(1, RoundingMode.HALF_UP);
